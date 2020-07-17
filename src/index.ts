4ec4b9746fb23ff9c0ea33a2b0c91f630ff3256a
Miguel319 <miguelp90.319@gmail.com>
2020-07-16 21:38:34 -0400

    Initial commit: server setup

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+/*Main dependencies */
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import mongoose  from "mongoose";
+
+const app = express();
+app.use(cors());
+dotenv.config();
+
+const BASE_URL: string = "api/v1";
+
+// Mongosose configuration
+mongoose.connect(String(process.env.DB_URI), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    // Seeder
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+// Routes
+// app.use(`${BASE}`)
+
+
+const PORT = 3001 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Server listening...");
+});

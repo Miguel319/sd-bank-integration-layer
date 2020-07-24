89e143a3215e8aeb561e0bbd369db7b8ae35e6ee
Thais Contreras <thaisc98@hotmail.es>
2020-07-23 20:05:26 -0400

    [new]  fixing urls

--- src/routes/loan.routes.ts
+++ src/routes/loan.routes.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
-import { getLoanById, getAllLoansByUser } from '../controllers/loan.controller';
+import { getLoanById, getAllLoansByUser, createLoan } from '../controllers/loan.controller';
 
 const  loanRouter: Router  = Router();
 
 loanRouter.route("/:user_id").get(getAllLoansByUser);
 loanRouter.route("/:_id").get(getLoanById);
+loanRouter.post("",createLoan);
 
 export default loanRouter;

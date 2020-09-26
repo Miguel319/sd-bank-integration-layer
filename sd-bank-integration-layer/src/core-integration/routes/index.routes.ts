f48c4c53ffc8bd5ed9de5ad6ffc93c1a4c27ea75
Srestituyo <sebastianrestituyo@gmail.com>
2020-09-26 12:03:27 -0400

    Adding Tipo de Transaccion EndPoint on Integration Layer

--- sd-bank-integration-layer/src/core-integration/routes/index.routes.ts
+++ sd-bank-integration-layer/src/core-integration/routes/index.routes.ts
@@ -2,6 +2,7 @@ import clienteRouter from "./cliente.routes";
 import prestamoRouter from "./prestamo.routes";
 import perfilRouter from "./perfil.routes";
 import cuentaRouter from "./cuenta.routes";
+import tipoTransaccion from "./tipo-de-transaccion.routes";
 
 const coreIntegrationRoutes = (app: any): void => {
   const BASE_URL: string = "/api/v1/core";
@@ -10,6 +11,8 @@ const coreIntegrationRoutes = (app: any): void => {
   app.use(`${BASE_URL}/prestamos`, prestamoRouter);
   app.use(`${BASE_URL}/perfiles`, perfilRouter);
   app.use(`${BASE_URL}/cuentas`, cuentaRouter);
+  app.use(`${BASE_URL}/tipo-de-transaccion`, tipoTransaccion);
+
 };
 
 export default coreIntegrationRoutes;

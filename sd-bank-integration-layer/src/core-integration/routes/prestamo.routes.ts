de4507c9e5042d9c51e231777fad3f0a38316a2c
Luis Miguel Rosa <miguelrosa0105@gmail.com>
2020-10-10 17:23:57 -0400

    Update prestamos.routes

--- sd-bank-integration-layer/src/core-integration/routes/prestamo.routes.ts
+++ sd-bank-integration-layer/src/core-integration/routes/prestamo.routes.ts
@@ -1,14 +1,15 @@
 import {
     getPrestamos,
     createPrestamo,
-    getPrestamoById
+    getPrestamoById,
+    updatePrestamo
   } from "./../controllers/prestamo.controller";
   import { Router } from "express";
   
   const prestamoRouter: Router = Router();
   
   prestamoRouter.route("").get(getPrestamos).post(createPrestamo);
-  prestamoRouter.route("/:_id").get(getPrestamoById)
+  prestamoRouter.route("/:_id").get(getPrestamoById).put(updatePrestamo)
   
   
   export default prestamoRouter;
